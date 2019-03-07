a9590ce52b6a9f4d7770e01f5427fe825c3b9ae4
Turnerj <git@turnerj.com>
2019-03-07 22:30:35 +1030

    Added basic tags test

---
+++ test/DevTo.Api.Tests/TagTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevTo.Api.Tests
+{
+	[TestClass]
+	public class TagTests : TestBase
+	{
+		[TestMethod]
+		public async Task GetTags()
+		{
+			var tagService = new TagService(BaseUri, HttpClient);
+			var tags = await tagService.GetTagsAsync();
+			Assert.IsNotNull(tags);
+		}
+	}
+}
