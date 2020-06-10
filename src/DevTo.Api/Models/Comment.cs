f18915297ac134baaffb9330901280591cd176ce
Facundo Giuliani <facundo.giuliani@gmail.com>
2020-06-10 01:16:57 -0300

    Add Comments API

---
+++ src/DevTo.Api/Models/Comment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DevTo.Api.Models
+{
+	public class Comment
+	{
+		[JsonProperty("id")]
+		public string ComentId { get; set; }
+		[JsonProperty("body_html")]
+		public string BodyHTML { get; set; }
+		[JsonProperty("user")]
+		public User User { get; set; }
+
+		[JsonProperty("children")]
+		public IEnumerable<Comment> Children { get; set; }
+	}
+}
