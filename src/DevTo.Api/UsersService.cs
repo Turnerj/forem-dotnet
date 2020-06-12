c28180846b2abfc4884d79994f78c20b663772d7
Facundo Giuliani <facundo.giuliani@gmail.com>
2020-06-12 20:34:19 -0300

    Add Users API

---
+++ src/DevTo.Api/UsersService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Forem.Api.Models;
+
+namespace Forem.Api
+{
+	public class UsersService : ApiService, IUsersApi
+	{
+		public UsersService(Uri baseUri, HttpClient httpClient) : base(baseUri, httpClient) { }
+
+		public Task<User> GetUserAsync(int id) => GetAsync<User>($"/api/users/{id}");
+
+		public Task<User> GetUserAsync(string username)
+			=> GetAsync<User>("/api/users/by_username", new Dictionary<string, object>
+				{
+				  { "url", username },
+				});
+
+		public Task<User> GetAuthenticatedUserAsync(string apiKey) => GetAsync<User>($"/api/users/me", null, apiKey);
+	}
+}
---
+++ tests/DevTo.Api.Tests/UsersApiTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Forem.Api.Tests
+{
+	[TestClass]
+	public class UsersApiTests : TestBase
+	{
+		[TestMethod]
+		public async Task GetUserById()
+		{
+			var usersService = new UsersService(BaseUri, HttpClient);
+			var user = await usersService.GetUserAsync(1);
+			Assert.IsNotNull(user);
+		}
+
+		[TestMethod]
+		public async Task GetUserByUsername()
+		{
+			var usersService = new UsersService(BaseUri, HttpClient);
+			var user = await usersService.GetUserAsync("ben");
+			Assert.IsNotNull(user);
+		}
+
+		[TestMethod, ExpectedException(typeof(ApiException))]
+		public async Task GetAuthenticatedUser_WithInvalidApiKeyThrowsException()
+		{
+			var usersService = new UsersService(BaseUri, HttpClient);
+			var user = await usersService.GetAuthenticatedUserAsync("NotARealApiKey");
+		}
+	}
+}
