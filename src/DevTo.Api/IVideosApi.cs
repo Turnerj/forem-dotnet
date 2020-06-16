c1673de1bb46965fb0b5c16f1af25228ac184896
Facundo Giuliani <facundo.giuliani@gmail.com>
2020-06-15 21:15:57 -0300

    Add Videos API

---
+++ src/DevTo.Api/IVideosApi.cs
@@ -0,0 +1,17 @@
+﻿using Forem.Api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Forem.Api
+{
+	public interface IVideosApi
+	{
+		/// <summary>
+		/// Retrieve a list of articles that are uploaded with a video.
+		/// </summary>
+		/// <param name="page">Pagination page.</param>
+		/// <param name="perPage">Page size (the number of items to return per page).</param>
+		/// <returns></returns>
+		Task<IEnumerable<Video>> GetVideosAsync(int page, int perPage);
+	}
+}
---
+++ tests/DevTo.Api.Tests/VideosApiTests.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forem.Api.Tests
+{
+	[TestClass]
+	public class VideosApiTests : TestBase
+	{
+		[TestMethod]
+		public async Task GetVideos()
+		{
+			var videosService = new VideosService(BaseUri, HttpClient);
+			var videos = await videosService.GetVideosAsync();
+			Assert.IsTrue(videos.Any());
+		}
+
+		[TestMethod]
+		public async Task GetVideosWithPagination()
+		{
+			var videosService = new VideosService(BaseUri, HttpClient);
+			var firstPageOfVideos = await videosService.GetVideosAsync(1, 2);
+			var secondPageOfVideos = await videosService.GetVideosAsync(2, 2);
+			Assert.IsTrue(firstPageOfVideos.Any());
+			Assert.IsTrue(secondPageOfVideos.Any());
+			Assert.AreNotEqual(firstPageOfVideos.First().VideoId, secondPageOfVideos.First().VideoId);
+		}
+	}
+}
