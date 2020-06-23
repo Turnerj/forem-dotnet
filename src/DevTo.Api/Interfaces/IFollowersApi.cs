1293a309dfcae4cc473d36bc17e7b2201873a2fc
Facundo Giuliani <facundo.giuliani@gmail.com>
2020-06-22 21:12:54 -0300

    Add Followers API

---
+++ src/DevTo.Api/Interfaces/IFollowersApi.cs
@@ -0,0 +1,18 @@
+﻿using Forem.Api.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Forem.Api.Interfaces
+{
+	public interface IFollowersApi
+	{
+		/// <summary>
+		/// Retrieves a list of the followers they have.
+		/// </summary>
+		/// <param name="apiKey">The API key associated to the account.</param>
+		/// <param name="page">Pagination page.</param>
+		/// <param name="perPage">Page size (the number of items to return per page).</param>
+		/// <returns></returns>
+		Task<IEnumerable<Follower>> GetUserFollowersAsync(string apiKey, int page, int perPage);
+	}
+}
---
+++ src/DevTo.Api/Services/FollowersService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Forem.Api.Interfaces;
+using Forem.Api.Models;
+
+namespace Forem.Api.Services
+{
+	public class FollowersService : ApiService, IFollowersApi
+	{
+		public FollowersService(Uri baseUri, HttpClient httpClient) : base(baseUri, httpClient) { }
+
+		public Task<IEnumerable<Follower>> GetUserFollowersAsync(string apiKey, int page = 1, int perPage = 80)
+			=> GetAsync<IEnumerable<Follower>>("/api/followers/users", new Dictionary<string, object>
+				{
+					{ "page", page },
+					{ "per_page", perPage },
+				}, apiKey);
+	}
+}
